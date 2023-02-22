b9fc356bc441768d4dbd23e36a66297eef06d62b
avocad06 <miggo2704@gmail.com>
2023-02-23 02:45:04 +0900

    add: BOJ-2146.다리만들기.py

--- python/BOJ/230220/2146_다리만들기.py
+++ python/BOJ/230220/2146_다리만들기.py
@@ -1,8 +1,12 @@
 """ 
-1. 
+1. 두 개의 bfs를 진행한다.
+2. bfs가 한 번 종료되면 섬 한 개를 다 돈 것
+3. 각각의 섬별로 다른 섬까지의 최소 거리를 측정한 거리 저장 배열을 만든다.
+4. 전역변수를 활용
 """
 from collections import deque
-from pprint import pprint
+from pprint import pprint as print
+import sys
 
 
 N = int(input())
@@ -33,21 +37,35 @@ def bfs_1(r, c, start):
                 queue.append((nr, nc))
 
 # 번호 붙이기
+# 섬의 개수이기도 함
 cnt = 1
 for r in range(N):
     for c in range(N):
         if maps[r][c] == 1:
+            # 땅을 찾았다면 시작 지점부터 번호 매기기
+            maps[r][c] = cnt + 1
             bfs_1(r, c, cnt + 1)
             cnt += 1
 
-checkList = [[-1] * N for _ in range(N)]
-# 가장 짧은 공백 세기
-# print(checkList)
+print(maps)
 
-def bfs_2(r, c, start):
+# 가장 짧은 공백 세기
+def bfs_2(start):
+    
+    global result
     queue = deque()
-    queue.append((r, c))
-    checkList[r][c] = 0
+
+    # 각 섬에 대응하는 checkList 생성
+    checkList = [[-1] * N for _ in range(N)]
+    
+    # 섬에 해당하는 영역의 번호를 0으로 초기화
+    for r in range(N):
+        for c in range(N):
+                # start와 같은 섬이라면,
+                if maps[r][c] == start:
+                    queue.append((r, c))
+                    # 0으로 초기화
+                    checkList[r][c] = 0
     
     while queue:
         r, c = queue.popleft()
@@ -59,18 +77,12 @@ def bfs_2(r, c, start):
             if nr < 0 or nr >= N or nc < 0 or nc >= N:
                 continue
             
-            # 섬이라면
-            if maps[nr][nc]:
-                # start와 같은 섬이라면,
-                if maps[nr][nc] == start:
-                    # 0으로 초기화
-                    checkList[nr][nc] = 0
-                    continue
-                
-                # start와 다른 섬이라면,(다른 섬을 만났음)
-                else:
-                    # 섬으로 만들고
-                    checkList[nr][nc] = 0
+            # 섬은 섬인데 start와 다른 섬이라면,(다른 섬을 만났음)
+            if maps[nr][nc] and maps[nr][nc] != start:
+                # print(start)
+                # print(checkList)
+                result = min(result, checkList[r][c])
+                return
             
             # 바다라면
             # 바다이고, 방문하지 않았다면,
@@ -79,14 +91,9 @@ def bfs_2(r, c, start):
                 checkList[nr][nc] = checkList[r][c] + 1
                 queue.append((nr, nc))
                 
-result = N ** 2
-for r in range(N):
-    for c in range(N):
-        if maps[r][c]:
-            tmp = bfs_2(r, c, maps[r][c])
-            if tmp:
-                print(tmp)
-                min(result, tmp)
+result = sys.maxsize
 
-pprint(checkList)
-
+for num in range(1, cnt):
+    bfs_2(num + 1)
+    
+print(result)
