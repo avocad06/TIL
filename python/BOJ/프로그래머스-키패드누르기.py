7d76cd399107afd0ded8cdfa0ed1f20a25c25393
avocad06 <miggo2704@gmail.com>
2023-03-26 23:35:58 +0900

    add: 키패드누르기.py

---
+++ python/BOJ/프로그래머스-키패드누르기.py
@@ -0,0 +1,11 @@
+# 67256
+
+# 규칙
+rules = [(1, "L"), (4, "L"), (7, "L"), (3, "R"), (6, "R"), (9, "R")]
+
+# 입력값
+numbers = map(int, input())
+numbers= list(numbers)
+hand = input()
+
+print(numbers, hand)
